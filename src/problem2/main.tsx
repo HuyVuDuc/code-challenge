880aecd54ad402d4ba014edceced2fc5a9455b79
huyvd <14122536+HuyVuDuc@users.noreply.github.com>
2026-05-07 11:12:45 +0700

    fix mock server to run on pages

--- src/problem2/main.tsx
+++ src/problem2/main.tsx
@@ -1,27 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { I18nextProvider } from "react-i18next";
-import { store } from "@/app/store";
-import { i18n } from "@/app/i18n";
-import { App } from "@/App";
+import {Provider} from "react-redux";
+import {I18nextProvider} from "react-i18next";
+import {store} from "@/app/store";
+import {i18n} from "@/app/i18n";
+import {App} from "@/App";
 import "@/index.css";
-import { startMockWorker } from "@/mocks/browser";
+import {startMockWorker} from "@/mocks/browser";
 
 async function bootstrap() {
-  if (import.meta.env.DEV) {
     await startMockWorker();
-  }
 
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
-      </Provider>
-    </React.StrictMode>,
-  );
+    ReactDOM.createRoot(document.getElementById("root")!).render(
+        <React.StrictMode>
+            <Provider store={store}>
+                <I18nextProvider i18n={i18n}>
+                    <App/>
+                </I18nextProvider>
+            </Provider>
+        </React.StrictMode>,
+    );
 }
 
 void bootstrap();
